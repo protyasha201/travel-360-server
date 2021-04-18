400f98e248bd5f06c537dfc8fd48981ae92ff19b
protyasha201 <protyasha0999@gmail.com>
2021-04-18 15:14:55 +0600

    added more

--- index.js
+++ index.js
@@ -22,75 +22,99 @@ client.connect(err => {
     app.post('/addService', (req, res) => {
         const services = req.body;
         servicesCollection.insertOne(services)
-        .then(result => {
-            res.send(result.insertedCount > 0)
-        })
-        .then(err => {
-            console.log(err);
-        })
+            .then(result => {
+                res.send(result.insertedCount > 0)
+            })
+            .then(err => {
+                console.log(err);
+            })
     })
 
     app.post('/addAdmins', (req, res) => {
         const admin = req.body;
         adminsCollection.insertOne(admin)
-        .then(result => {
-            res.send(result.insertedCount > 0)
-        })
-        .then(err => {
-            console.log(err);
-        })
+            .then(result => {
+                res.send(result.insertedCount > 0)
+            })
+            .then(err => {
+                console.log(err);
+            })
     })
 
     app.post('/bookings', (req, res) => {
         const bookings = req.body;
         bookingsCollection.insertOne(bookings)
-        .then(result => {
-            res.send(result.insertedCount > 0)
-        })
-        .then(err => {
-            console.log(err);
-        })
+            .then(result => {
+                res.send(result.insertedCount > 0)
+            })
+            .then(err => {
+                console.log(err);
+            })
+    })
+
+    app.get('/orderedBookings', (req, res) => {
+        bookingsCollection.find({})
+            .toArray((err, documents) => {
+                res.send(documents)
+            })
+    })
+
+    app.get('/myBookings', (req, res) => {
+        bookingsCollection.find({ email: req.query.email })
+            .toArray((err, documents) => {
+                res.send(documents)
+            })
     })
 
     app.post('/addReview', (req, res) => {
         const reviews = req.body;
         reviewsCollection.insertOne(reviews)
-        .then(result => {
-            res.send(result.insertedCount > 0)
-        })
-        .then(err => {
-            console.log(err);
-        })
+            .then(result => {
+                res.send(result.insertedCount > 0)
+            })
+            .then(err => {
+                console.log(err);
+            })
     })
 
     app.get('/services', (req, res) => {
         servicesCollection.find({})
-        .toArray((err, documents) => {
-            res.send(documents)
-        })
+            .toArray((err, documents) => {
+                res.send(documents)
+            })
     })
 
     app.get('/admins', (req, res) => {
         adminsCollection.find({})
-        .toArray((err, documents) => {
-            res.send(documents)
-        })
+            .toArray((err, documents) => {
+                res.send(documents)
+            })
     })
 
     app.get('/reviews', (req, res) => {
         reviewsCollection.find({})
-        .toArray((err, documents) => {
-            res.send(documents)
-        })
+            .toArray((err, documents) => {
+                res.send(documents)
+            })
     })
 
 
-    app.get('/bookTravel/:id', (req,res) => {
+    app.get('/bookTravel/:id', (req, res) => {
         const id = req.params.id;
-        servicesCollection.find({_id: ObjectID(id)})
-        .toArray((err, documents) => {
-            res.send(documents[0]);
-        })
+        servicesCollection.find({ _id: ObjectID(id) })
+            .toArray((err, documents) => {
+                res.send(documents[0]);
+            })
+    })
+
+    app.delete('/delete/:id', (req, res) => {
+        servicesCollection.deleteOne({ _id: ObjectID(req.params.id) })
+            .then(result => {
+                // console.log(result);
+            })
+            .catch(err => {
+                console.log(err);
+            })
     })
 
     app.get('/', (req, res) => {
